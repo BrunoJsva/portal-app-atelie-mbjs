cb3c66a452c66e154c3f379dd660aeb72da1fb3c
Bruno de Jesus Silva <bruno.silva@MJV.net>
2023-10-02 18:26:09 -0300

    criando projeto

---
+++ src/main/java/br/com/appateliembjs/appateliembjs/AppAtelieMbjsApplication.java
@@ -0,0 +1,13 @@
+package br.com.appateliembjs.appateliembjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAtelieMbjsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAtelieMbjsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/appateliembjs/appateliembjs/AppAtelieMbjsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.appateliembjs.appateliembjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAtelieMbjsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
